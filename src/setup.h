d205047dbc09c06fff95a69d023c036874b45f4e
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-09-05 09:28:12 -0500

    Add some pin definitions and function headers.

--- src/setup.h
+++ src/setup.h
@@ -5,12 +5,20 @@
 
 
 /* Definitions for switch_1 and switch_2 input data. */
+#define PA0_IDR (GPIOA->IDR & 0x01)
 #define PA1_IDR (GPIOA->IDR & 0x02)
 #define PA2_IDR (GPIOA->IDR & 0x04)
 
 
 /* Hard-coded to setup specific pins. May be changed in future to allow parameters. */
 void setup_pins(void);
+
+
+/* Setup interrupt */
+void setup_interrupts(void);
+
+
+/* Interrupt service routines will go here. */
 
 
 #endif
