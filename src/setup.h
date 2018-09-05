e23ca43499a735d71c809835fab7d4fcfa3ae7f8
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-09-05 09:22:28 -0500

    Update setup.h

--- src/setup.h
+++ src/setup.h
@@ -5,8 +5,8 @@
 
 
 /* Definitions for switch_1 and switch_2 input data. */
-#define PA1_IDR ((GPIOA->IDR & 0x02) >> 1)
-#define PA2_IDR ((GPIOA->IDR & 0x04) >> 2)
+#define PA1_IDR (GPIOA->IDR & 0x02)
+#define PA2_IDR (GPIOA->IDR & 0x04)
 
 
 /* Hard-coded to setup specific pins. May be changed in future to allow parameters. */
