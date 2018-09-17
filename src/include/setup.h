f431fa8055693eddf332becfcc09f46b53135e89
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-09-17 10:41:49 -0500

    Update setup.h

--- src/include/setup.h
+++ src/include/setup.h
@@ -29,35 +29,6 @@
 #define PC98_DOUT_CLR   (~(0x0A << (2*8)))  /* Bits to clear for PC[9:8] MODER (output). */
 
 
-/* Definitions for specific pin MODER modes. */
-#define P0_MODE_INPUT 0x00000003
-#define P1_MODE_INPUT 0x0000000C
-#define P2_MODE_INPUT 0x00000030
-
-
-/* Definitions for GPIOA input data registers. */
-#define PA0_IDR ((uint16_t) (GPIOA->IDR & 0x01))
-#define PA1_IDR ((uint16_t) (GPIOA->IDR & 0x02))
-#define PA2_IDR ((uint16_t) (GPIOA->IDR & 0x04))
-
-
-/* GPIOC ODR for reading LED states. */
-#define PC8_ODR ((uint16_t) (GPIOC->ODR & 0x100))
-#define PC9_ODR ((uint16_t) (GPIOC->ODR & 0x200))
-
-
-/* Interrupt setup definitions. */
-#define SYSCFG_EXTICR1   (SYSCFG->EXTICR[0])
-#define EXTI_IMR         (EXTI->IMR)
-#define EXTI_FTSR        (EXTI->FTSR)
-#define EXTI_RTSR        (EXTI->RTSR)
-#define EXTICRn_PA(x)    ((uint32_t) (0xF << (4*(x))))
-#define EXTICRn_PA_EN(x) ((uint32_t) (0x0 << (4*(x))))
-#define EXTI0_IMR_MASK   ((uint32_t) 0x01)
-#define EXTI1_IMR_MASK   ((uint32_t) 0x02)
-#define EXTI_EDGE_EN(n)  ((uint32_t) (0x01 << (n)))
-
-
 /* Function for writing to a GPIO ODR. */
 void write_to_odr(GPIO_TypeDef gpio, uint16_t value, uint16_t shift, uint16_t bitmask);
 
