d0ceaf7f9553757dc3c3786f3dd6f65d62df0366
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-09-17 10:40:07 -0500

    Update pin_definitions.h

--- src/include/pin_definitions.h
+++ src/include/pin_definitions.h
@@ -12,5 +12,11 @@
 #define PIN_DEFINITIONS_H
 
 
+/* GPIO clock enable definitions. */
+#define RCC_AHBENR   (RCC->AHBENR)
+#define GPIOA_RCC_EN 0x01
+#define GPIOB_RCC_EN 0x02
+#define GPIOC_RCC_EN 0x04
+
 
 #endif
