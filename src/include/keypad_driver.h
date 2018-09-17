9a4baad15bb5241902ae60af077430a8ef2a4e50
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-09-17 09:15:23 -0500

    Update keypad_driver.h

--- src/include/keypad_driver.h
+++ src/include/keypad_driver.h
@@ -34,19 +34,19 @@ typedef struct
 } *keypad_interface;   
 
 
-/* Helper function for writing a value to a GPIO's ODR using the BSRR. */
-void write_to_odr(GPIO_TypeDef gpio, uint16_t value);
-
-
-/* Reads the key pressed on the keypad and returns the number it represents. */
-uint16_t read_keypress(void);
-
-
-/* Function for creating a keypad interface. */
+/* Function for creating a keypad interface - only non-static function in file. */
 keypad_interface create_keypad_interface(GPIO_TypeDef gpio, uint16_t r_bitfield, uint16_t c_bitfield);
 
 
+/* Helper function for writing a value to a GPIO's ODR using the BSRR. */
+static void write_to_odr(GPIO_TypeDef gpio, uint16_t value);
+
+
+/* Reads the key pressed on the keypad and returns the number it represents. */
+static uint16_t read_keypress(void);
+
+
 /* Decode row-col pair to know what key was pressed. */
-uint16_t decode_row_col(uint16_t row, uint16_t col);
+static uint16_t decode_row_col(uint16_t row, uint16_t col);
 
 #endif
