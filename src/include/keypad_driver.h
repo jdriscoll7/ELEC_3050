0c4b34faf47ad9c517ebe6fef044e51da7951d0f
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-09-17 10:03:16 -0500

    Update keypad_driver.h

--- src/include/keypad_driver.h
+++ src/include/keypad_driver.h
@@ -46,7 +46,7 @@ keypad_interface create_keypad_interface(GPIO_TypeDef gpio, uint16_t r_bitfield,
 
 
 /* Helper function for writing a value to a GPIO's ODR using the BSRR. */
-static void write_to_odr(GPIO_TypeDef gpio, uint16_t value, uint16_t bitmask);
+static void write_to_odr(GPIO_TypeDef gpio, uint16_t value, uint16_t shift, uint16_t bitmask);
 
 
 /* Reads the key pressed on the keypad and returns the number it represents. */
