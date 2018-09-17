d6892070f04b6ceca15adf766b1124bcfac75d30
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-09-17 10:41:08 -0500

    Update pin_definitions.h

--- src/include/pin_definitions.h
+++ src/include/pin_definitions.h
@@ -19,4 +19,16 @@
 #define GPIOC_RCC_EN 0x04
 
 
+/* Interrupt setup definitions. */
+#define SYSCFG_EXTICR1   (SYSCFG->EXTICR[0])
+#define EXTI_IMR         (EXTI->IMR)
+#define EXTI_FTSR        (EXTI->FTSR)
+#define EXTI_RTSR        (EXTI->RTSR)
+#define EXTICRn_PA(x)    ((uint32_t) (0xF << (4*(x))))
+#define EXTICRn_PA_EN(x) ((uint32_t) (0x0 << (4*(x))))
+#define EXTI0_IMR_MASK   ((uint32_t) 0x01)
+#define EXTI1_IMR_MASK   ((uint32_t) 0x02)
+#define EXTI_EDGE_EN(n)  ((uint32_t) (0x01 << (n)))
+
+
 #endif
