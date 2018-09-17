19da26dae5f5f844cfcbd1a0700297342054c511
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-09-17 10:39:02 -0500

    file for pin definitions

---
+++ src/include/pin_definitions.h
@@ -0,0 +1,16 @@
+/* - File: pin_definitions.h 
+
+       - Description
+           - Contains all pin definitions shared by files, like RCC register locations
+             and GPIO locations.
+             
+       - Author: Joe Driscoll 
+*/
+
+
+#ifndef PIN_DEFINITIONS_H
+#define PIN_DEFINITIONS_H
+
+
+
+#endif
