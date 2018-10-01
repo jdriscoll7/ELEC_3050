93db9183ab22b29c03783bf254157ddc218054e6
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-10-01 09:44:09 -0500

    Update pwm_gen.h

--- src/include/pwm_gen.h
+++ src/include/pwm_gen.h
@@ -9,7 +9,7 @@
 #ifndef PWM_GEN_H
 #define PWM_GEN_H
 
-
+/* Decode keypad keys to duty cycles (mapping defined by x -> x/10). */
 #define KEY_TO_DUTY_CYCLE(key) ((float) (((float) (key)) / 10.0))
 
 
