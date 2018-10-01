1b45139111dd272c1926cefc734f9e9bed6ef711
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-10-01 14:03:50 -0500

    Update timing_functions.h

--- src/include/timing_functions.h
+++ src/include/timing_functions.h
@@ -36,7 +36,7 @@ static uint8_t time_tenths = 0;
 
 /* Array of function pointers to make interrupt handler flexible. */
 uint16_t function_count;
-void (**function_ptr_array)(void);
+void (**tim10_function_array)(void);
 void set_TIM10_functions(void (**new_function)(void), size_t size);
 
 
