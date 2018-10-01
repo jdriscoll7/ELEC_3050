218d5de45eff760063b2731af4ef3588cea7e29d
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-10-01 10:33:42 -0500

    Create pwm_gen.c

---
+++ src/code/pwm_gen.c
@@ -0,0 +1,45 @@
+/* - File: pwm_gen.h 
+       - Description
+           - Provides a convenient interface to a PWM.
+             
+       - Author: Joe Driscoll 
+*/
+
+
+/* Static variable that defines current duty cycle (along with getter/setter functions). */
+static float duty_cycle;
+
+
+/* Setter for duty_cycle variable. */
+void set_duty_cycle(float new_value)
+{
+    duty_cycle = new_value;
+}
+
+
+/* Getter for duty_cycle variable. */
+float get_duty_cycle(void)
+{
+    return duty_cycle;
+}
+
+
+/* Function for determining output value of pwm. */
+uint16_t get_pwm_output(float time)
+{
+    /* What percent of the period is completed? */
+    float portion_completed = time * SWITCHING_FREQ;
+    
+    /* Off if time is past the on portion of pulse, on otherwise. */
+    return ((portion_completed > duty_cycle) ? 0 : 1)
+}
+
+
+/* Provides pin, clock, and initialization setup for the PWM. */
+void setup_pwm(void)
+{
+    
+}
+
+
+#endif
