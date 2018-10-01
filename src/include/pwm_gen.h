40dff7b6dfbd623d6a9d3fe3063923650232105d
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-10-01 09:59:31 -0500

    Update pwm_gen.h

--- src/include/pwm_gen.h
+++ src/include/pwm_gen.h
@@ -10,12 +10,26 @@
 #define PWM_GEN_H
 
 
+#include <stdint.h>
+#include <stdbool.h>
+#include "STM32L1xx.h"
+#include "pin_definitions.h"
+
+
+/* Output PWM on PA6. */
+#define PWM_OUTPUT_PIN 6
+
+
 /* 1kHz switching frequency. */
 #define SWITCHING_FREQ 1000
 
 
 /* Decode keypad keys to duty cycles (mapping defined by x -> x/10). */
 #define KEY_TO_DUTY_CYCLE(key) ((float) (((float) (key)) / 10.0))
+
+
+/* Provides pin, clock, and initialization setup for the PWM. */
+void setup_pwm(void);
 
 
 #endif
