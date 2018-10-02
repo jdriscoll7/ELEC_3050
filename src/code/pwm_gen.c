c507723e5335f1f1102fddc4090e969d2e94623b
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-10-02 16:16:45 -0500

    In-lab fixes

--- src/code/pwm_gen.c
+++ src/code/pwm_gen.c
@@ -11,7 +11,7 @@
 
 
 /* Static variable that defines current duty cycle (along with getter/setter functions). */
-static uint32_t inverse_duty_cycle;
+static float duty_cycle;
 
 
 /* Calculates ARR for a switching frequency. */
@@ -24,14 +24,14 @@ uint16_t calculate_arr(uint32_t frequency)
 /* Calculates CCR given arr. */
 uint16_t calculate_ccr(uint16_t arr)
 {
-    return ((uint16_t) (arr / inverse_duty_cycle));
+    return ((uint16_t) ((arr + 1) * duty_cycle));
 }
 
 
 /* Setter for duty_cycle variable. */
 void set_duty_cycle(float new_duty_cycle)
 {
-    inverse_duty_cycle = (uint16_t)(1 / new_duty_cycle);
+    duty_cycle = new_duty_cycle;
     
     uint16_t arr = calculate_arr(SWITCHING_FREQ);
     
@@ -42,7 +42,7 @@ void set_duty_cycle(float new_duty_cycle)
 /* Getter for duty_cycle variable. */
 float get_duty_cycle(void)
 {
-    return (1/inverse_duty_cycle);
+    return duty_cycle;
 }
 
 
