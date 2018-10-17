beca4e51df0b1ad9bb473315c509089b3da05916
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-10-17 16:28:25 -0500

    Update pwm_gen.h

--- src/include/pwm_gen.h
+++ src/include/pwm_gen.h
@@ -32,7 +32,6 @@ uint16_t calculate_arr(uint32_t frequency);
 uint16_t calculate_ccr(uint16_t arr);
 
 
-
 /* Static variable that defines current duty cycle (along with getter/setter functions). */
 static float duty_cycle;
 void set_duty_cycle(float new_duty_cycle);
