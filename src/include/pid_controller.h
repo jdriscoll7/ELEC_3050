eaf256620f9fdde362f210c962379d6b9c08c8c2
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-11-13 13:49:23 -0600

    Update pid_controller.h

--- src/include/pid_controller.h
+++ src/include/pid_controller.h
@@ -11,7 +11,12 @@
 /* Voltage constants for each speed (based on tachometer amplitude measurements). */
 /* Ranges from 0.45V to 1.1 V */
 #define NUM_SPEEDS 10
+
 static uint32_t speed_to_amplitude_table[NUM_SPEEDS] = {471859, 547590, 623320, 699051, 774781, 850512, 926242, 1001973, 1077703, 1153434};
+
+
+/* Miscillaneous definitions. */
+#define ERROR_BUFFER_SIZE 3
 
 
 void controller_step(uint32_t new_data);
