287ad6a333fd8e7360ab9204ae2d724a16ee55dd
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-11-13 13:44:54 -0600

    Update pid_controller.h

--- src/include/pid_controller.h
+++ src/include/pid_controller.h
@@ -11,10 +11,7 @@
 /* Voltage constants for each speed (based on tachometer amplitude measurements). */
 /* Ranges from 0.45V to 1.1 V */
 #define NUM_SPEEDS 10
-uint32_t speed_to_amplitude_table[NUM_SPEEDS] = {471859, 547590, 623320, 699051, 774781, 850512, 926242, 1001973, 1077703, 1153434};
-
-
-uint16_t desired_speed;
+static uint32_t speed_to_amplitude_table[NUM_SPEEDS] = {471859, 547590, 623320, 699051, 774781, 850512, 926242, 1001973, 1077703, 1153434};
 
 
 void controller_step(uint32_t new_data);
