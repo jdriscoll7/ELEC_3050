a47120c427c7a4b498a1bee7e8a65dc21b42931e
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-11-13 14:02:05 -0600

    Update pid_controller.c

--- src/code/pid_controller.c
+++ src/code/pid_controller.c
@@ -1,4 +1,46 @@
 #include "pid_controller.h"
+#include "pwm_gen.h"
 
-static uint16_t data_acquisition_buffer[DATA_ACQUISITION_BUFFER_SIZE];
-static uint32_t data_acquisition_index = 0;
+
+/* Holds desired speed. */
+static uint16_t desired_speed;
+
+
+/* Used to hold past error measurements. */
+uint32_t error_buffer[ERROR_BUFFER_SIZE];
+uint8_t error_buffer_indx = 0;
+
+
+/* Holds last control action. */
+uint32_t last_control_action = 0;
+
+
+/* Min and max helper macros. */
+#define MIN(a,b) (((a) < (b)) ? (a) : (b))
+#define MAX(a,b) (((a) > (b)) ? (a) : (b))
+
+
+void controller_step(uint32_t new_error_data)
+{
+    /* Update error buffer. */
+    error_buffer[error_buffer_indx] = new_error_data;
+    
+    /* Update and make control action. */
+    last_control_action = last_control_action + A0*error_buffer[0] - A1*error_buffer[1] + A2*error_buffer[2];
+    
+    /* Saturate control action to fit within duty cycle. */
+    last_control_action = MIN(MAX(0, last_control_action), 100);
+    
+    /* Actually make control action. */
+    set_duty_cycle(last_control_action);
+    
+    /* Increment error buffer index. */
+    error_buffer_indx++;
+}
+
+
+
+void set_desired_speed(uint16_t new_speed)
+{
+    desired_speed = new_speed;
+}
