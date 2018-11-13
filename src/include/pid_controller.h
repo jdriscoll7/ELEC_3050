4d4d7801ca18d59ba65f7263402f03b370eb3709
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-11-13 13:57:15 -0600

    Update pid_controller.h

--- src/include/pid_controller.h
+++ src/include/pid_controller.h
@@ -12,6 +12,7 @@
 /* Ranges from 0.45V to 1.1 V */
 #define NUM_SPEEDS 10
 
+
 static uint32_t speed_to_amplitude_table[NUM_SPEEDS] = {471859, 547590, 623320, 699051, 774781, 850512, 926242, 1001973, 1077703, 1153434};
 
 
