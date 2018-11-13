608289f36388373d90042bcf74f17d8188f02c63
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-11-13 13:40:20 -0600

    Update tachometer_driver.h

--- src/include/tachometer_driver.h
+++ src/include/tachometer_driver.h
@@ -23,23 +23,11 @@
 #define MA_WINDOW_SIZE 10
 
 
-/* Controller parameters. */
-#define A0 1
-#define A1 1
-#define A2 1
-
-
 /* Reference voltage for ADC is hard-set to 3V. */
 #define ADC_VOLTAGE_REF 3
 #define ADC_BITS 12
 #define ADC_BITS_RAISED ((float) (1 << ADC_BITS))
 #define ADC_STEP (((float) ADC_VOLTAGE_REF) / (ADC_BITS_RAISED - 1))
-
-
-/* Voltage constants for each speed (based on tachometer amplitude measurements). */
-/* Ranges from 0.45V to 1.1 V */
-#define NUM_SPEEDS 10
-uint32_t speed_to_amplitude_table[NUM_SPEEDS] = {471859, 547590, 623320, 699051, 774781, 850512, 926242, 1001973, 1077703, 1153434};
 
 
 /* Moving average filter for measurement filtering. */
