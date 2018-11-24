2157d4a87bcceee9fb11741313366d2527df5285
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-11-23 18:38:32 -0600

    Update bcd_timer.h

--- src/include/bcd_timer.h
+++ src/include/bcd_timer.h
@@ -10,14 +10,6 @@
 #define BCD_TIMER_H
 
 
-/* Timer type definitions (easier than managing global variable). */
-typedef struct bcd_timer
-{
-    uint8_t ones_place;
-    uint8_t tenths_place;
-} bcd_timer_t;
-
-
 /* Increments timer - returns BCD time as 8-bit number (upper four bits as ones place, lower four as tenths place). */
 uint8_t increment_timer(void);
 
