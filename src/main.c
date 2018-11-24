92527ba11ddd7fbccfb77fc81a6464c2a1722fc3
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-11-23 18:58:05 -0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -11,6 +11,11 @@
 #include "setup.h"
 
 
+/* Define on and off keys for bcd timer. */
+#define ON_KEY  0xF
+#define OFF_KEY 0xE
+
+
 int main()
 {
     setup_pins();
@@ -26,8 +31,23 @@ int main()
         {
             /* Decode key and set the duty cycle. */
             uint16_t key_pressed = get_key_pressed();
-            set_desired_speed(key_pressed);
-
+            
+            /* See if key controls bcd timer or motor speed. */
+            switch (key_pressed)
+            {
+                case ON_KEY:
+                    bcd_timer_on();
+                    break;
+                    
+                case OFF_KEY:
+                    bcd_timer_off();
+                    break;
+                    
+                default:
+                    set_desired_speed(key_pressed);
+            }
+            
+            /* Clear key pressed flag to make keypad controller work (tells it that key has been read). */
             clear_key_pressed_flag();
         }
     }
