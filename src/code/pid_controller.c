abd853d10f964ccc518735275dcadb9048d81da5
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2018-11-30 09:24:21 -0600

    Update pid_controller.c

--- src/code/pid_controller.c
+++ src/code/pid_controller.c
@@ -8,12 +8,12 @@ static uint16_t desired_speed;
 
 
 /* Used to hold past error measurements. */
-int32_t error_buffer[ERROR_BUFFER_SIZE];
+int64_t error_buffer[ERROR_BUFFER_SIZE];
 uint8_t error_buffer_indx = 0;
 
 
 /* Holds last control action. */
-int32_t last_control_action = 0;
+int64_t last_control_action = 0;
 
 
 /* Min and max helper macros. */
@@ -28,24 +28,46 @@ void controller_step(uint32_t speed_data)
     {
         set_duty_cycle(0);
     }
+    else if (desired_speed == 10)
+    {
+        set_duty_cycle(100);
+    }
     else
     {
-    /* Update error buffer. */
-    error_buffer[error_buffer_indx] = (int32_t) (((int32_t) speed_to_amplitude_table[desired_speed]) - ((int32_t) speed_data));
-    
-    /* Update and make control action. */
-    last_control_action = last_control_action + (((A0*error_buffer[MOD(error_buffer_indx, ERROR_BUFFER_SIZE)]) 
-                                              -   (A1*error_buffer[MOD(error_buffer_indx - 1, ERROR_BUFFER_SIZE)]) 
-                                              +   (A2*error_buffer[MOD(error_buffer_indx - 2, ERROR_BUFFER_SIZE)])) >> 10);
-    
-    /* Saturate control action to fit within duty cycle. */
-    last_control_action = MIN(MAX(0, last_control_action), 100);
-    
-    /* Actually make control action. */
-    set_duty_cycle(last_control_action);
-    
-    /* Increment error buffer index. */
-    error_buffer_indx = MOD(error_buffer_indx + 1, ERROR_BUFFER_SIZE);
+        /* Update error buffer. */
+        error_buffer[error_buffer_indx] = (int32_t) (((int32_t) speed_to_amplitude_table[desired_speed]) - ((int32_t) speed_data));
+
+        #if TUNING_MODE
+        
+        /* Calculate A0, A1, A2 dynamically in terms of constants. */
+        int32_t new_A0 = Kp + (Ki / 200) + (100 * Kd);
+        int32_t new_A1 = Kp + (200 * Kd) - (Ki/200);
+        int32_t new_A2 = Kd * 100;
+        
+        /* Update and make control action. */
+        last_control_action = last_control_action + (((new_A0*error_buffer[MOD(error_buffer_indx, ERROR_BUFFER_SIZE)]) 
+                                                  -   (new_A1*error_buffer[MOD(error_buffer_indx - 1, ERROR_BUFFER_SIZE)]) 
+                                                  +   (new_A2*error_buffer[MOD(error_buffer_indx - 2, ERROR_BUFFER_SIZE)])) >> 10);
+
+        
+        #else
+        
+        /* Update and make control action normally. */
+        last_control_action = last_control_action + (((A0*error_buffer[MOD(error_buffer_indx, ERROR_BUFFER_SIZE)]) 
+                                                  -   (A1*error_buffer[MOD(error_buffer_indx - 1, ERROR_BUFFER_SIZE)]) 
+                                                  +   (A2*error_buffer[MOD(error_buffer_indx - 2, ERROR_BUFFER_SIZE)])) >> 10);
+
+        #endif
+        
+        
+        /* Saturate control action to fit within duty cycle. */
+        last_control_action = MIN(MAX(0, last_control_action), 100);
+
+        /* Actually make control action. */
+        set_duty_cycle(last_control_action);
+
+        /* Increment error buffer index. */
+        error_buffer_indx = MOD(error_buffer_indx + 1, ERROR_BUFFER_SIZE);
     }
 }
 
